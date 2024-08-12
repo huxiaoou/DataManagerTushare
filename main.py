04363f4cf28a3653cf4f982453c3c617c116c3b4
huxiaoou <516984451@qq.com>
2024-08-12 10:53:09 +0800

    Add supparser for arguments;Refactor main.py

--- main.py
+++ main.py
@@ -2,91 +2,93 @@ import argparse
 
 
 def parse_args():
-    arg_parser = argparse.ArgumentParser(description="Project to download data from tushare")
-    arg_parser.add_argument(
+    arg_parser_main = argparse.ArgumentParser(description="Project to download data from tushare")
+    arg_parser_main.add_argument("--bgn", type=str, required=True)
+    arg_parser_main.add_argument("--stp", type=str, default=None)
+
+    arg_parser_subs = arg_parser_main.add_subparsers(
+        title="sub function",
+        dest="func",
+        description="use this argument to go to call different functions",
+    )
+
+    # func: download
+    arg_parser_sub = arg_parser_subs.add_parser(name="download", help="Download data from tushare and wind")
+    arg_parser_sub.add_argument(
         "--switch", type=str, required=True,
         choices=("fmd", "contract", "universe", "position", "basis"),
     )
-    arg_parser.add_argument("--bgn", type=str, required=True)
-    arg_parser.add_argument("--stp", type=str, default=None)
-    _args = arg_parser.parse_args()
+
+    # func: update
+    arg_parser_sub = arg_parser_subs.add_parser(name="update", help="Update data for database")
+    arg_parser_sub.add_argument(
+        "--switch", type=str, required=True,
+        choices=("fmd", "position", "basis"),
+    )
+
+    # --- parse args
+    _args = arg_parser_main.parse_args()
     return _args
 
 
 if __name__ == "__main__":
     from project_cfg import pro_cfg
-    from husfort.qcalendar import CCalendar
     from husfort.qlog import define_logger
+    from husfort.qcalendar import CCalendar
 
     define_logger()
-
-    args = parse_args()
     calendar = CCalendar(calendar_path=pro_cfg.calendar_path)
 
-    if args.switch == "fmd":
-        from data_engines import CDataEngineTushareFutDailyMd
+    args = parse_args()
+    bgn, stp = args.bgn, args.stp or calendar.get_next_date(args.bgn, shift=1)
 
-        engine = CDataEngineTushareFutDailyMd(
-            save_root_dir=pro_cfg.daily_data_root_dir,
-            save_data_info=pro_cfg.futures_md,
-        )
-        engine.download_data_range(
-            bgn_date=args.bgn,
-            stp_date=args.stp or calendar.get_next_date(args.bgn),
-            calendar=calendar,
-        )
-    elif args.switch == "contract":
-        from data_engines import CDataEngineTushareFutDailyCntrcts
+    if args.func == "download":
+        if args.switch == "fmd":
+            from data_engines import CDataEngineTushareFutDailyMd
 
-        engine = CDataEngineTushareFutDailyCntrcts(
-            save_root_dir=pro_cfg.daily_data_root_dir,
-            save_data_info=pro_cfg.futures_contracts,
-            md_data_info=pro_cfg.futures_md,
-        )
-        engine.download_data_range(
-            bgn_date=args.bgn,
-            stp_date=args.stp or calendar.get_next_date(args.bgn),
-            calendar=calendar,
-        )
-    elif args.switch == "universe":
-        from data_engines import CDataEngineTushareFutDailyUnvrs
+            engine = CDataEngineTushareFutDailyMd(
+                save_root_dir=pro_cfg.daily_data_root_dir,
+                save_data_info=pro_cfg.futures_md,
+            )
+            engine.download_data_range(bgn_date=bgn, stp_date=stp, calendar=calendar)
+        elif args.switch == "contract":
+            from data_engines import CDataEngineTushareFutDailyCntrcts
 
-        engine = CDataEngineTushareFutDailyUnvrs(
-            save_root_dir=pro_cfg.daily_data_root_dir,
-            save_data_info=pro_cfg.futures_universe,
-            cntrcts_data_info=pro_cfg.futures_contracts,
-            exceptions={"SCTAS.INE"},
-        )
-        engine.download_data_range(
-            bgn_date=args.bgn,
-            stp_date=args.stp or calendar.get_next_date(args.bgn),
-            calendar=calendar,
-        )
-    elif args.switch == "position":
-        from data_engines import CDataEngineTushareFutDailyPos
+            engine = CDataEngineTushareFutDailyCntrcts(
+                save_root_dir=pro_cfg.daily_data_root_dir,
+                save_data_info=pro_cfg.futures_contracts,
+                md_data_info=pro_cfg.futures_md,
+            )
+            engine.download_data_range(bgn_date=bgn, stp_date=stp, calendar=calendar)
+        elif args.switch == "universe":
+            from data_engines import CDataEngineTushareFutDailyUnvrs
 
-        engine = CDataEngineTushareFutDailyPos(
-            save_root_dir=pro_cfg.daily_data_root_dir,
-            save_data_info=pro_cfg.futures_pos,
-            exchanges=pro_cfg.futures_exchanges,
-        )
-        engine.download_data_range(
-            bgn_date=args.bgn,
-            stp_date=args.stp or calendar.get_next_date(args.bgn),
-            calendar=calendar,
-        )
-    elif args.switch == "basis":
-        from data_engines import CDataEngineWindFutDailyBasis
+            engine = CDataEngineTushareFutDailyUnvrs(
+                save_root_dir=pro_cfg.daily_data_root_dir,
+                save_data_info=pro_cfg.futures_universe,
+                cntrcts_data_info=pro_cfg.futures_contracts,
+                exceptions={"SCTAS.INE"},
+            )
+            engine.download_data_range(bgn_date=bgn, stp_date=stp, calendar=calendar)
+        elif args.switch == "position":
+            from data_engines import CDataEngineTushareFutDailyPos
 
-        engine = CDataEngineWindFutDailyBasis(
-            save_root_dir=pro_cfg.daily_data_root_dir,
-            save_data_info=pro_cfg.futures_basis,
-            unvrs_data_info=pro_cfg.futures_universe,
-        )
-        engine.download_data_range(
-            bgn_date=args.bgn,
-            stp_date=args.stp or calendar.get_next_date(args.bgn),
-            calendar=calendar,
-        )
-    else:
-        raise ValueError(f"switch = {args.switch} is illegal")
+            engine = CDataEngineTushareFutDailyPos(
+                save_root_dir=pro_cfg.daily_data_root_dir,
+                save_data_info=pro_cfg.futures_pos,
+                exchanges=pro_cfg.futures_exchanges,
+            )
+            engine.download_data_range(bgn_date=bgn, stp_date=stp, calendar=calendar)
+        elif args.switch == "basis":
+            from data_engines import CDataEngineWindFutDailyBasis
+
+            engine = CDataEngineWindFutDailyBasis(
+                save_root_dir=pro_cfg.daily_data_root_dir,
+                save_data_info=pro_cfg.futures_basis,
+                unvrs_data_info=pro_cfg.futures_universe,
+            )
+            engine.download_data_range(bgn_date=bgn, stp_date=stp, calendar=calendar)
+        else:
+            raise ValueError(f"switch = {args.switch} is illegal")
+    elif args.func == "update":
+        print("sub parser = 'update'")
---
+++ run_all.ps1
@@ -0,0 +1,8 @@
+$bgn_date = "20120104"
+$stp_date = "20240812"
+
+python main.py --bgn $bgn_date --stp $stp_date download --switch fmd
+python main.py --bgn $bgn_date --stp $stp_date download --switch contract
+python main.py --bgn $bgn_date --stp $stp_date download --switch universe
+python main.py --bgn $bgn_date --stp $stp_date download --switch position
+python main.py --bgn $bgn_date --stp $stp_date download --switch basis
