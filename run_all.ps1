0a2fc4ad142bfc2bee54bcc9b653719c39333b0d
huxiaoou <516984451@qq.com>
2024-12-04 13:09:04 +0800

    Update dates

--- run_all.ps1
+++ run_all.ps1
@@ -1,7 +1,7 @@
 #$bgn_date = "20120104"
 #$stp_date = "20240812"
 $bgn_date = "20241101"
-$stp_date = "20241119"
+$stp_date = "20241201"
 
 python main.py --bgn $bgn_date --stp $stp_date download --switch fmd
 python main.py --bgn $bgn_date --stp $stp_date download --switch contract
@@ -9,7 +9,7 @@ python main.py --bgn $bgn_date --stp $stp_date download --switch universe
 python main.py --bgn $bgn_date --stp $stp_date download --switch position
 python main.py --bgn $bgn_date --stp $stp_date download --switch basis
 python main.py --bgn $bgn_date --stp $stp_date download --switch stock
-#python main.py --bgn $bgn_date --stp $stp_date download --switch minute # update from juejin by month
+python main.py --bgn $bgn_date --stp $stp_date download --switch minute # update from juejin by month
 
 python main.py --bgn $bgn_date --stp $stp_date update --switch fmd
 python main.py --bgn $bgn_date --stp $stp_date update --switch position
