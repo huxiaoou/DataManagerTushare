6b0c1ea4202236923a50f3bae7bdac5f8e21bcd3
huxiaoou <516984451@qq.com>
2025-03-04 16:23:52 +0800

    Update end date

--- run_all.ps1
+++ run_all.ps1
@@ -1,16 +1,16 @@
-$bgn_date = "20250206"
-$stp_date = "20250217"
+$bgn_date = "20250205"
+$stp_date = "20250301"
 
-python main.py --bgn $bgn_date --stp $stp_date download --switch fmd
-python main.py --bgn $bgn_date --stp $stp_date download --switch contract
-python main.py --bgn $bgn_date --stp $stp_date download --switch universe
-python main.py --bgn $bgn_date --stp $stp_date download --switch position
-python main.py --bgn $bgn_date --stp $stp_date download --switch basis
-python main.py --bgn $bgn_date --stp $stp_date download --switch stock
+#python main.py --bgn $bgn_date --stp $stp_date download --switch fmd
+#python main.py --bgn $bgn_date --stp $stp_date download --switch contract
+#python main.py --bgn $bgn_date --stp $stp_date download --switch universe
+#python main.py --bgn $bgn_date --stp $stp_date download --switch position
+#python main.py --bgn $bgn_date --stp $stp_date download --switch basis
+#python main.py --bgn $bgn_date --stp $stp_date download --switch stock
+#
+#python main.py --bgn $bgn_date --stp $stp_date update --switch fmd
+#python main.py --bgn $bgn_date --stp $stp_date update --switch position
+#python main.py --bgn $bgn_date --stp $stp_date update --switch basis
+#python main.py --bgn $bgn_date --stp $stp_date update --switch stock
 
-python main.py --bgn $bgn_date --stp $stp_date update --switch fmd
-python main.py --bgn $bgn_date --stp $stp_date update --switch position
-python main.py --bgn $bgn_date --stp $stp_date update --switch basis
-python main.py --bgn $bgn_date --stp $stp_date update --switch stock
-
-#python main.py --bgn $bgn_date --stp $stp_date download --switch minute # update from juejin by month
+python main.py --bgn $bgn_date --stp $stp_date download --switch minute # update from juejin by month
