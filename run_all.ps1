f1f67e4a8288f4d27a2b300a2c14cb092367cd42
huxiaoou <516984451@qq.com>
2025-04-08 12:33:05 +0800

    Update dates in run_all.ps1

--- run_all.ps1
+++ run_all.ps1
@@ -1,5 +1,5 @@
-$bgn_date = "20250205"
-$stp_date = "20250301"
+$bgn_date = "20250303"
+$stp_date = "20250401"
 
 #python main.py --bgn $bgn_date --stp $stp_date download --switch fmd
 #python main.py --bgn $bgn_date --stp $stp_date download --switch contract
