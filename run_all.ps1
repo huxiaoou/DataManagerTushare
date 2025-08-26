d5df83ca62f41b8cfbef6126c30b5a40d39e038f
huxiaoou <516984451@qq.com>
2025-08-26 16:48:17 +0800

    Update dates in run_all.ps1 for July, 2025

--- run_all.ps1
+++ run_all.ps1
@@ -1,15 +1,15 @@
-$bgn_date = "20250601"
-$stp_date = "20250701"
+$bgn_date = "20250701"
+$stp_date = "20250801"
 
 python main.py --bgn $bgn_date --stp $stp_date download --switch fmd
 python main.py --bgn $bgn_date --stp $stp_date download --switch contract
 python main.py --bgn $bgn_date --stp $stp_date download --switch universe
-#python main.py --bgn $bgn_date --stp $stp_date download --switch position
+python main.py --bgn $bgn_date --stp $stp_date download --switch position
 python main.py --bgn $bgn_date --stp $stp_date download --switch basis
 python main.py --bgn $bgn_date --stp $stp_date download --switch stock
 
 python main.py --bgn $bgn_date --stp $stp_date update --switch fmd
-#python main.py --bgn $bgn_date --stp $stp_date update --switch position
+python main.py --bgn $bgn_date --stp $stp_date update --switch position
 python main.py --bgn $bgn_date --stp $stp_date update --switch basis
 python main.py --bgn $bgn_date --stp $stp_date update --switch stock
 
